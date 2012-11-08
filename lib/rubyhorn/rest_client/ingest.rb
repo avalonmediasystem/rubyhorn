589ac6c2f4bf6e0c3f7f45f18c9b64cfa50844f2
Phuong Dinh <phuongdh@gmail.com>
2012-11-08 09:53:31 -0500

    Added a new ingest point

--- lib/rubyhorn/rest_client/ingest.rb
+++ lib/rubyhorn/rest_client/ingest.rb
@@ -7,5 +7,17 @@ module Rubyhorn::RestClient
       uri = "ingest/addMediaPackage" + (params["workflow"].nil? ? "" : "/#{params["workflow"]}")
       return Rubyhorn::Workflow.from_xml multipart_post(uri, file, params)
     end
+
+    # Adds a mediapackage and starts ingesting, using an URL as the resource
+    def addMediaPackageWithUrl(params)
+      logger.debug "<<<<< IM IN URL MeTHOd >>>>>"
+      
+      raise "Missing required field url" unless params.include? "url"
+      raise "Missing required field flavor" unless params.include? "flavor"
+      raise "Missing required field title" unless params.include? "title"
+
+      uri = "ingest/addMediaPackage" + (params["workflow"].nil? ? "" : "/#{params["workflow"]}")
+      return Rubyhorn::Workflow.from_xml post(uri, params)
+    end
   end
 end
