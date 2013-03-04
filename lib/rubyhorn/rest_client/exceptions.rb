1aa8c1421b31702f5ff022dbc016eb0753ed74a0
Adam Hallett <adam.t.hallett@gmail.com>
2013-03-04 15:17:09 -0600

    ruby horn raises exceptions on failure

---
+++ lib/rubyhorn/rest_client/exceptions.rb
@@ -0,0 +1,22 @@
+module Rubyhorn::RestClient
+  module Exceptions
+    class RubyhornException < StandardError
+    end
+
+    class ServerError < RubyhornException
+      attr_reader :status_code
+      def initialize(status_code)
+        @status_code = status_code
+        super("Server responded with status code: #{status_code}.")
+      end
+    end
+
+    class MissingRequiredParams < RubyhornException
+      attr_reader :params
+      def initialize(params)
+        @params = params
+        super("You failed to include #{params.join(', ')} in your request.")
+      end
+    end
+  end
+end
--- lib/rubyhorn/rest_client/ingest.rb
+++ lib/rubyhorn/rest_client/ingest.rb
@@ -10,14 +10,11 @@ module Rubyhorn::RestClient
 
     # Adds a mediapackage and starts ingesting, using an URL as the resource
     def addMediaPackageWithUrl(params)
-      logger.debug "<<<<< IM IN URL MeTHOd >>>>>"
-      
-      raise "Missing required field url" unless params.include? "url"
-      raise "Missing required field flavor" unless params.include? "flavor"
-      raise "Missing required field title" unless params.include? "title"
-
-      uri = "ingest/addMediaPackage" + (params["workflow"].nil? ? "" : "/#{params["workflow"]}")
-      return Rubyhorn::Workflow.from_xml post(uri, params)
+      missing_params = ['url','flavor','title','filename','workflow'].collect{|field| field if ! params.include?(field) }.compact
+      raise Rubyhorn::RestClient::Exceptions::MissingRequiredParams.new(missing_params) if missing_params.present?
+      uri = "ingest/addMediaPackage"
+      uri += "/#{params["workflow"]}" unless params["workflow"].nil?
+      Rubyhorn::Workflow.from_xml post(uri, params)
     end
   end
 end
